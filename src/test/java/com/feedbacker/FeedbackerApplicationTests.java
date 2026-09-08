8cc5232926f9d79e47a2476c84d88ea66f37f2d0
kimminhyeong <minhyeong63@gmail.com>
2026-09-08 12:23:06 +0900

    initialize feedbacker project

---
+++ src/test/java/com/feedbacker/FeedbackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedbacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
