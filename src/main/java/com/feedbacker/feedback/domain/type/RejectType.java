ef8da0df755d00f733e2fa06644d5f5fe3b7cc6a
thisisdibellob <thisisdibellob@gmail.com>
2026-09-26 01:59:28 +0900

    feat: 피드백 거절 사유 유형 추가

--- src/main/java/com/feedbacker/feedback/domain/type/RejectType.java
+++ src/main/java/com/feedbacker/feedback/domain/type/RejectType.java
@@ -1,5 +1,8 @@
 package com.feedbacker.feedback.domain.type;
 
 public enum RejectType {
+    IRRELEVANT_ANSWER,  // 질문과 무관한 답변
+    LOW_EFFORT_ANSWER,  // 성의 없이 작성된 답변
+    TEST_NOT_PERFORMED, // 테스트를 실제로 진행하지 않음
     OTHER
 }
